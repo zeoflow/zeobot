b05c7a15121e1f698ce20483d3d1bb66ae6fdb91
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-01 14:44:44 +0100

    license header added

--- res/templates/index.js
+++ res/templates/index.js
@@ -1,4 +1,20 @@
-const { handlebars } = require('hbs')
+/**
+ * Copyright 2020 ZeoFlow SRL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ const { handlebars } = require('hbs')
 
 // Register a githubHost global helper to make links respect the GHE_HOST env var
 handlebars.registerHelper('githubHost', () => process.env.GHE_HOST || 'github.com')
